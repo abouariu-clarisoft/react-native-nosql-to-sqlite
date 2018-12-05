f1c4efda02ffabec66083f0c8eadcc8bc82a0026
Andrei Bouariu <andrei.bouariu@clarisoft.com>
2018-12-05 12:33:23 +0200

    Send import and export progress using React Event Emitter

--- ios/RNNosqlToSqlite.m
+++ ios/RNNosqlToSqlite.m
@@ -4,27 +4,31 @@
 
 @implementation RNNosqlToSqlite
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
 
 - (dispatch_queue_t)methodQueue {
     return dispatch_get_main_queue();
+}
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"ImportProgress", @"ExportProgress"];
 }
 
 RCT_EXPORT_METHOD(configureDatabaseWithName:(NSString *)name encryptionKey:(NSString *)key config:(NSDictionary *)config ) {
     [[DBController sharedInstance] configureDatabaseWithName:name encryptionKey:key config:config];
 }
 
-RCT_EXPORT_METHOD(importDataWithProgress:(RCTResponseSenderBlock)progress callback:(RCTResponseSenderBlock)callback) {
-    [[DBController sharedInstance] importDataWithProgress:^(NSString * _Nonnull collection, NSInteger progress, NSInteger total) {
-        progress(@[collection, @(progress), @(total)]);
+RCT_EXPORT_METHOD(importData:(RCTResponseSenderBlock)callback) {
+    [[DBController sharedInstance] importDataWithProgress:^(NSString * _Nonnull collection, NSInteger processed, NSInteger total) {
+        [self sendEventWithName:@"ImportProgress" body:@[collection, @(processed), @(total)]];
     } completion:^(NSArray * _Nonnull errors) {
         callback(@[errors.count > 0 ? errors : [NSNull null]]);
     }];
 }
 
-RCT_EXPORT_METHOD(exportDataWithProgress:(RCTResponseSenderBlock)progress callback:(RCTResponseSenderBlock)callback) {
-    [[DBController sharedInstance] exportDataWithProgress:^(NSString * _Nonnull collection, NSInteger progress, NSInteger total) {
-        progress(@[collection, @(progress), @(total)]);
+RCT_EXPORT_METHOD(exportData:(RCTResponseSenderBlock)callback) {
+    [[DBController sharedInstance] exportDataWithProgress:^(NSString * _Nonnull collection, NSInteger processed, NSInteger total) {
+        [self sendEventWithName:@"ExportProgress" body:@[collection, @(processed), @(total)]];
     } completion:^(NSArray * _Nonnull errors) {
         callback(@[errors.count > 0 ? errors : [NSNull null]]);
     }];
@@ -32,14 +36,14 @@ RCT_EXPORT_METHOD(exportDataWithProgress:(RCTResponseSenderBlock)progress callba
 
 RCT_EXPORT_METHOD(performSelect:(NSString *)query completion:(RCTResponseSenderBlock)callback) {
     [[DBController sharedInstance] performSelect:query
-                                      completion:^(NSString * _Nullable error, NSArray * _Nullable result) {
+                                      completion:^(NSError * _Nullable error, NSArray * _Nullable result) {
                                           callback(@[error ? error : [NSNull null], result]);
-    }];
+                                      }];
 }
 
 RCT_EXPORT_METHOD(performUpdate:(NSString *)query completion:(RCTResponseSenderBlock)callback) {
     [[DBController sharedInstance] performUpdate:query
-                                      completion:^(NSString * _Nullable error) {
+                                      completion:^(NSError * _Nullable error) {
                                           callback(@[error ? error : [NSNull null]]);
                                       }];
 }
